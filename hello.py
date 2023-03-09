392e00ca8f724b957491fbf92839e7d71536dc2e
Ankam Ravi Kumar <14858025+techarkit@users.noreply.github.com>
2023-03-09 15:00:43 +0530

    Create hello.py
    Hello Python Program

---
+++ hello.py
@@ -0,0 +1,11 @@
+# This program Says Hello And Asks for my Name
+
+print ('Hello World')
+print ('What is your name?')
+myName = input()
+print('It is a Good to meet you, ' + myName)
+print('The length of your name is: characters')
+print(len(myName))
+print('What is your age?')
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a Year.')
