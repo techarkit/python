79ad23b6727424d84eb27be62841e621d8df6ef9
Ankam Ravi Kumar <14858025+techarkit@users.noreply.github.com>
2023-03-09 16:33:27 +0530

    Update if_else_example.py

--- if_else_example.py
+++ if_else_example.py
@@ -1,7 +1,7 @@
 # Example of If else statement
 
 password = 'MyPassword'
-if password == 'MyPassword'
+if password == 'MyPassword':
     print('Your In.')
 else:
     print('Bad Password, Try Again!.')
