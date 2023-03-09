1ec9f4805932f8bf2fba87b9d7cf83b469f3612d
Ankam Ravi Kumar <14858025+techarkit@users.noreply.github.com>
2023-03-09 15:11:42 +0530

    Update hello.py
    Added explain

--- hello.py
+++ hello.py
@@ -1,11 +1,21 @@
 # This program Says Hello And Asks for my Name
 
 print ('Hello World')
+
 print ('What is your name?')
 myName = input()
 print('It is a Good to meet you, ' + myName)
 print('The length of your name is:')
 print(len(myName))
+
 print('What is your age?')
 myAge = input()
 print('You will be ' + str(int(myAge) + 1) + ' in a Year.')
+
+### The python code will execute the code from top to buttom instruction by instruction.
+### Line is written with pound sign is just a comment the line is excluded from execution
+### Blank lines between the program are ignored
+### Values passed to functions = Arguments
+### its a simple print function and store input to variable
+### Line 9 is len function which count the length of the given input
+### Line 13 String concatenate
