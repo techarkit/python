17b63d8e1ec6b10901a9f7fa1393e8e12e55457d
Ankam Ravi Kumar <14858025+techarkit@users.noreply.github.com>
2023-03-13 12:14:38 +0530

    Create

---
+++ if_else_if_example.py
@@ -0,0 +1,11 @@
+## If else if example code
+name = 'Ravi'
+age = '50'
+if name == 'Ravi':
+    print('Hi Ravi, How are you?')
+elif age < 15:
+    print('You are not Ravi.')
+elif age > 40:
+    print('Hey You Ravi!, Hello')
+elif age > 60:
+    print('You are not Ravi, Grandpa')
