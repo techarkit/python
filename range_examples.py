266a51f9297b27a344d32ff0aa526fa79587c658
Ankam Ravi Kumar <14858025+techarkit@users.noreply.github.com>
2023-03-21 13:30:14 +0530

    Create range_examples.py

---
+++ range_examples.py
@@ -0,0 +1,9 @@
+## Range fuction examples
+print('My Name is Ravi')
+for j in range(5, 10):
+  print('Print 5 to 10 Times' + str(j))
+
+## Print values 2*number 
+print('My Name is Ravi')
+for i in range(0, 10, 2):
+  print('Print 2 into 5 Times' + str(i))
